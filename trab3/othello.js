c5e4c5c3e172574789c7a4a7f1d58d558422a409
Lucas Helio <lucasheliosr@hotmail.com>
2022-11-03 17:43:44 -0300

    algumas alteraçõesde css e implementação de função

--- trab3/othello.js
+++ trab3/othello.js
@@ -2,27 +2,52 @@ window.onload = function() {
     makeBoard();
 }
 
+var cont =0; //contador pra saber quem joga na prox rodada
+
+
+function createcircle(td) {
+    let circle = document.createElement('div');
+    
+    if(cont%2==0)
+    circle.style.backgroundColor = '#e9e627';//amarelo
+    
+    else
+    circle.style.backgroundColor = 'white';//branco
+    
+    circle.className = 'circle';
+    td.appendChild(circle);
+
+    cont++;     //contador pra saber quem joga na prox rodada
+}
+
+
+
 function makeBoard() {
-    let board = document.getElementById('board');
+    
+    let board = document.getElementById('board');    
     
     for(let i = 0; i < 8; i++) {
-
-        let tr = document.createElement('tr');
+        let tr = document.createElement('tr');       
         
         for(let j = 0; j < 8; j++) {
-            
             let td = document.createElement('td');
+            td.id = (i*10)+j;           
             tr.appendChild(td);
 
-            if( i == 4 && j == 4 ) {
-                let circle = document.createElement('div');
-                circle.style.backgroundColor = 'black';
-                circle.className = 'circle';
-                td.appendChild(circle);
+            if( td.id == 44 ) {                
+                createcircle(td);
             }
-
         }    
-
         board.appendChild(tr);
     }
 }
+
+
+
+//tentando descobrir como pegar ids que não sei quais são
+function msg(){
+    alert("vc clicou na circunferencia");
+}
+
+// document.addEventListener('click', createcircle(td));
+document.getElementById("44").addEventListener("click", msg);
